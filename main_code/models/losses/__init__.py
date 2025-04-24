314b999267888533a5de3b458d91882001d27ee1
8078qwe <xh1m22@std.uestc.edu.cn>
2025-04-24 22:19:45 +0800

    Delete main_code directory

--- main_code/models/losses/__init__.py
+++
@@ -1,3 +0,0 @@
-from .seg_losses import BCEFocalLoss, dice_loss, HOITransDiceLoss
-
-__all__ = ['BCEFocalLoss', 'dice_loss', 'HOITransDiceLoss']
--- main_code/models/roi_heads/roi_head.py
+++
@@ -1,9 +0,0 @@
-
-from mmdet.models import HEADS, StandardRoIHead
-
-
-@HEADS.register_module()
-class RoIHead(StandardRoIHead):
-    def __init__(self, param, **kwargs):
-        super().__init__(**kwargs)
-        self.param = self.param
